4413cba710cde354cd0521bd8eb969512511d7b5
Mukesh <mukesh@ingenioustechie.com>
2016-01-16 21:58:02 +0530

    changed siteurl name

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Mukesh'
 SITENAME = u'Learning the hard way!'
-SITEURL = 'https://www.ingenioustechie.com'
+SITEURL = 'https://ingenioustechie.com'
 
 PATH = 'content'
 
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ingeioustechie.com'
+SITEURL = 'http://ingenioustechie.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
